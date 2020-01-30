bf6901a4ce1110b3e2c2fc0dd798d41fe62152a9
unknown <pablojpba@gmail.com>
2020-01-30 12:48:14 -0300

    addeed test network and started api dev

--- sp/sp-api.py
+++ sp/sp-api.py
@@ -8,13 +8,33 @@ sumoBinary = checkBinary('sumo-gui')
 
 sumoCmd = [sumoBinary, '-c', os.path.join(sumocfg.networks, r"simple_network\simple.sumocfg")]
 
+parkings_occ = None
+
+def check_parking_lots():
+	global parkings_occ
+	for parking, occupancy in parkings_occ.items():
+		if occupancy is None:
+			return parking
+	return None
+
 def apply_parking_logic(vehicleHandler):
+	for vehicle in vehicleHandler.getIDList():
+		currentEdge = vehicleHandler.getRoadID( vehicle )
+		if currentEdge == 'pEntrance': #Reroute to find a parking spot.
+			check_parking_lots()
 	pass
 
+
+def store_parking_lots(edgeHandler):
+	global parkings_occ
+	parkings = [edge for edge in edgeHandler.getIDList() if "pe" in edge and "pex" not in edge]
+	parkings_occ = {key:None for key in parkings}
 
 def simulate():
 	traci.start( sumoCmd )
 	vehicleHandler = traci.vehicle
+	edgeHandler = traci.edge
+	store_parking_lots( edgeHandler )
 	step = 0
 	while step < 1000:
 		traci.simulationStep()

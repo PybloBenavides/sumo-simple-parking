5166c1cb69c21526a60abc8c4afb49e9b5bc551c
unknown <pablojpba@gmail.com>
2020-02-04 16:19:35 -0300

    getting to the zones ok

--- sp/mall.py
+++ sp/mall.py
@@ -24,21 +24,22 @@ sumoBinary = checkBinary('sumo-gui')
 sumoCmd = [sumoBinary, '-c', os.path.join(sumocfg.networks, r"mall\mall.sumocfg")]
 
 
-PARKING_ENTRANCE = 'ParkingEntrance'
+PARKING_ENTRANCE = 'networkEntrance'
 
 vehicle_handler = None
-rerouted_vehs = None
+rerouted_parkers = None
 parking_mall = None
 class ParkingMall():
 	def __init__(self):
 		self.zones = {}
-		self.capacity = -1
+		self.capacity = 0
 		self.vehicles_inside = 0
 	def add_zone(self, zone):
 		self.zones[zone.name] = zone
 	def set_capacity(self):
 		for zone in self.zones.keys():
 			self.capacity += len(self.zones[zone].parking_spots)
+		print(f"capacity: {self.capacity}")
 	def vehicle_enters(self): 
 		self.vehicles_inside += 1
 	def vehicle_leaves(self):
@@ -52,17 +53,18 @@ class ParkingZone():
 	def __init__(self, name):
 		self.name = name
 		self.parking_spots = {}
-		
+		self.entrances = []	
 	def add_parking_spot(self, name):
-		self.parking_spots[name] = None
-
+		self.parking_spots[name] = None	
 	def __str__(self):
 		return self.name
 
 class Parker():
-	def __init__(self, name):
+	def __init__(self, name, parking_mall):
 		self.name = name
-		self.parking_preference = ['A','B'] if random.randint(0,10) > 3 else ['B','A']
+		zoneA = parking_mall.zones['A']
+		zoneB = parking_mall.zones['B']
+		self.parking_preference = [zoneA,zoneB] if random.randint(0,10) > 3 else [zoneB, zoneA]
 
 	def __str__(self):
 		return self.name
@@ -80,31 +82,37 @@ def store_parking_lots(traci):
 				parking_mall.add_zone( parking_zone )
 			parking_mall.zones[zone].add_parking_spot( parkingspot )
 	parking_mall.set_capacity()
-	# for zone in parking_mall.zones:
-	# 	print(zone)
-	# 	print(parking_mall.zones[zone].parking_spots)
-	# 	print("..")
+	parking_mall.zones['A'].entrances = ['A_Entrance_0', 'A_Entrance_1']
+	parking_mall.zones['B'].entrances = ['B_Entrance_0']
+
+def reroute_to_parking_zone( parker ):
+	global vehicle_handler, parking_mall
+	desired_zone = parker.parking_preference[0]
+	vehicle_handler.changeTarget( parker.name, desired_zone.entrances[0] )
+	#TODO: Change entrances[0] to shortest path entrance
 
 def apply_parking_logic( parking_entrance ):
-	global vehicle_handler, rerouted_vehs, parking_mall
-	for vehicle in vehicle_handler.getIDList():
-		current_edge = vehicle_handler.getRoadID( vehicle )
-		if current_edge == parking_entrance and vehicle not in rerouted_vehs:
-			if parking_mall.is_full():
-				continue
-			parker = Parker(vehicle)
-			print(f"tony parker preference {parker.parking_preference}")
+	global vehicle_handler, rerouted_parkers, parking_mall
+	if not parking_mall.is_full():
+		for vehicle in vehicle_handler.getIDList():
+			current_edge = vehicle_handler.getRoadID( vehicle )
+			if current_edge == parking_entrance and vehicle not in rerouted_parkers:
+				parker = Parker(vehicle, parking_mall)
+				print(f"tony parker preference {parker.parking_preference}")
+				reroute_to_parking_zone(parker)
+				rerouted_parkers.append( parker.name )
+
 
 
 
 def simulate():
-	global rerouted_vehs, vehicle_handler
-	rerouted_vehs = []
+	global rerouted_parkers, vehicle_handler
+	rerouted_parkers = {}
 	traci.start( sumoCmd )
 	store_parking_lots( traci )
 	step = 0
 	vehicle_handler = traci.vehicle
-	# rerouted_vehs = []
+	# rerouted_parkers = []
 	while step < 1000:
 		traci.simulationStep()
 		apply_parking_logic( PARKING_ENTRANCE )

5951359c3644a1fb5a4e061a2c564a4222bf9813
unknown <pablojpba@gmail.com>
2020-02-12 12:40:09 -0300

    parking ok, exiting parking not ok

--- sp/mall.py
+++ sp/mall.py
@@ -25,7 +25,12 @@ sumoCmd = [sumoBinary, '-c', os.path.join(sumocfg.networks, r"mall\mall.sumocfg"
 
 
 PARKING_ENTRANCE = 'networkEntrance'
+NETWORK_EXIT = 'networkExit'
+PARKING_EXIT = 'parkingExit'
+MIN_PARKING_TIME = 20
+MAX_PARKING_TIME = 50
 
+step = 0
 vehicle_handler = None
 rerouted_parkers = None
 parking_mall = None
@@ -56,6 +61,15 @@ class ParkingZone():
 		self.entrances = []	
 	def add_parking_spot(self, name):
 		self.parking_spots[name] = None	
+
+	def find_parking_spot(self):
+		for parking_spot, occupancy in self.parking_spots.items():
+			if occupancy is None:
+				return parking_spot
+		return None
+	def reserve_parking_spot(self, parking_spot, parker):
+		self.parking_spots[parking_spot] = parker
+
 	def __str__(self):
 		return self.name
 
@@ -66,6 +80,21 @@ class Parker():
 		zoneB = parking_mall.zones['B']
 		self.parking_preference = [zoneA,zoneB] if random.randint(0,10) > 3 else [zoneB, zoneA]
 		self.is_parking = False
+		self.parking_spot = None
+		self.end_parking_time = None
+	def park(self, step):
+		self.is_parking = True
+	def change_parking_preference(self):
+		self.parking_preference = list(reversed(self.parking_preference))
+	def choose_parking_spot(self, parking_spot):
+		self.parking_spot = parking_spot
+	
+	def free_parking_spot(self):
+		'''
+		Frees Parking spot for parker and also Parking zone
+		'''
+		zone = self.parking_preference[0]
+		zone.reserve_parking_spot(self.parking_spot, None)
 	def __str__(self):
 		return self.name
 
@@ -91,7 +120,41 @@ def reroute_to_parking_zone( parker ):
 	vehicle_handler.changeTarget( parker.name, desired_zone.entrances[0] )
 	#TODO: Change entrances[0] to shortest path entrance
 
-def apply_parking_logic( parking_entrance ):
+def reroute_to_parking_spot(parker, parking_spot):
+	global vehicle_handler, step
+	parker.choose_parking_spot(parking_spot)
+	vehicle_handler.changeTarget( parker.name, parking_spot )
+	print(f"Vehicle {parker.name} going into {parking_spot}")
+	parking_duration = random.randint( MIN_PARKING_TIME, MAX_PARKING_TIME )
+	vehicle_handler.setStop(parker.name, parking_spot, duration=parking_duration)
+	parker.end_parking_time = parking_duration + step
+	# parker.set_end_parking_time(parking_duration + step)
+	# parkings_occ[parking_spot] = (parker.name, parking_duration + step)
+
+def find_spot_in_zone(parker, parking_zone):
+	parking_spot = parking_zone.find_parking_spot()
+	if parking_spot is None:
+		return None
+	parking_zone.reserve_parking_spot(parking_spot, parker)
+	reroute_to_parking_spot(parker, parking_spot)
+	return parking_spot
+
+
+def exit_parking( parker ):
+	'''
+	Make vehicle exit Parking spot and Parking lot.
+	'''
+	global vehicle_handler, parking_mall
+	vehicle_handler.changeTarget( parker.name, NETWORK_EXIT )
+	print(f"rerouting {parker.name} to {NETWORK_EXIT}")
+	try:
+		vehicle_handler.resume(vehicle)
+		parker.free_parking_spot()
+		parkings_occ[parking_spot] = None
+	except:
+		return
+
+def apply_parking_logic( parking_entrance, step ):
 	global vehicle_handler, rerouted_parkers, parking_mall
 	if not parking_mall.is_full():
 		for vehicle in vehicle_handler.getIDList():
@@ -99,18 +162,36 @@ def apply_parking_logic( parking_entrance ):
 			if vehicle not in rerouted_parkers:
 				if current_edge == parking_entrance:
 					parker = Parker(vehicle, parking_mall)
-					print(f"tony parker preference {parker.parking_preference}")
 					reroute_to_parking_zone(parker)
 					rerouted_parkers[vehicle] = parker
+					parking_mall.vehicle_enters()
 			else:
 				parker = rerouted_parkers[vehicle]
-				if current_edge in self.parking_preference.entrances:
-					print("tony parker is in the entrance of its desired zone.")
+				parking_zone = parker.parking_preference[0]
+				if current_edge in parking_zone.entrances and parker.parking_spot is None:
+					spot = find_spot_in_zone(parker, parking_zone)
+					# print("tony parker is in the entrance of its desired zone.")
+					if spot is None:
+						parker.change_parking_preference()
+						reroute_to_parking_zone(parker)
+				elif current_edge == parker.parking_spot:
+					if not parker.is_parking: # parker just entered parking spot
+						parker.park(step)
+					else:
+						if step >= parker.end_parking_time:
+							exit_parking(parker)
+				elif current_edge == PARKING_EXIT:
+					parking_mall.vehicle_leaves()
+					rerouted_parkers.pop(vehicle)
+	else:
+		print("parking is fullerino")
+
+
 
 
 
 def simulate():
-	global rerouted_parkers, vehicle_handler
+	global rerouted_parkers, vehicle_handler, step
 	rerouted_parkers = {}
 	traci.start( sumoCmd )
 	store_parking_lots( traci )
@@ -119,7 +200,7 @@ def simulate():
 	# rerouted_parkers = []
 	while step < 1000:
 		traci.simulationStep()
-		apply_parking_logic( PARKING_ENTRANCE )
+		apply_parking_logic( PARKING_ENTRANCE, step )
 		#Your code
 		step += 1
 	traci.close()
